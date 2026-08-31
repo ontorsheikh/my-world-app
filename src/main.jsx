896c6c5c64509307c8eccdf3565077d8c3e8ff16
ontorsheikh <skontorsheikh1613@gmail.com>
2026-08-31 15:40:33 +0600

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+// Google Verification Meta Tag ডাইনামিক্যালি যুক্ত করা
+const meta = document.createElement("meta");
+meta.name = "google-site-verification";
+meta.content = "k02IoSwf94n2zzxPWIuTZ_F_APIg_KqYo5V4it6RlFM";
+document.head.appendChild(meta);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
