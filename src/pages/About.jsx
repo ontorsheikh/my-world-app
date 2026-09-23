3e27e0098a784c0ceb01bc71d903519c27441116
ontorsheikh <skontorsheikh1613@gmail.com>
2026-09-23 22:43:30 +0600

    added about editon

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,14 +6,16 @@ export default function About() {
       <BackButton />
       <h1>About Ontor's World Web</h1>
       <p>
-        World Web is an educational platform for exploring basic information
+        Ontor's World Web is an educational platform for exploring basic information
         about countries around the world. This website is built by Developer Md.
         Ontor Sheikh (mail: skontorsheikh1613@gmail.com)
+        <br /><br /> Md. Ashim Sheikh is appointed as a Chief Marketing Officer. He is also a co-founder.   
+
+      <br /><br />
+      Our purposes are helping people to explore the world info. We are confident to provide authenticated information about all countries in the world.
       </p>
-      <p>
-        Data is stored client-side for fast, serverless access and to make this
-        project easy to deploy on Netlify.
-      </p>
+      
+        
     </div>
   );
 }
